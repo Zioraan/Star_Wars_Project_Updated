f02cc3581a077bb533b0d8faba64240c54612033
Zioraan <ziondrago1@gmail.com>
2025-05-28 12:45:28 -0400

    added expanded fetch to show information on the home page

--- src/actions.js
+++ src/actions.js
@@ -4,35 +4,45 @@ export const useActions = () => {
   const { store, dispatch } = useGlobalReducer();
 
   const getPeople = async () => {
-    const response = await fetch("https://www.swapi.tech/api/people");
+    const response = await fetch(
+      "https://www.swapi.tech/api/people/?expanded=true"
+    );
     const data = await response.json();
     dispatch({ type: "add_people", payload: { people: data.results } });
     return data.results;
   };
 
   const getPlanets = async () => {
-    const response = await fetch("https://www.swapi.tech/api/planets");
+    const response = await fetch(
+      "https://www.swapi.tech/api/planets/?expanded=true"
+    );
     const data = await response.json();
     dispatch({ type: "add_planets", payload: { planets: data.results } });
     return data.results;
   };
 
   const getVehicles = async () => {
-    const response = await fetch("https://www.swapi.tech/api/vehicles");
+    const response = await fetch(
+      "https://www.swapi.tech/api/vehicles/?expanded=true"
+    );
     const data = await response.json();
     dispatch({ type: "add_vehicles", payload: { vehicles: data.results } });
     return data.results;
   };
 
   const getSpecies = async () => {
-    const response = await fetch("https://www.swapi.tech/api/species");
+    const response = await fetch(
+      "https://www.swapi.tech/api/species/?expanded=true"
+    );
     const data = await response.json();
     dispatch({ type: "add_species", payload: { species: data.results } });
     return data.results;
   };
 
   const getStarships = async () => {
-    const response = await fetch("https://www.swapi.tech/api/starships");
+    const response = await fetch(
+      "https://www.swapi.tech/api/starships/?expanded=true"
+    );
     const data = await response.json();
     dispatch({ type: "add_starships", payload: { starships: data.results } });
     return data.results;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,20 +3,70 @@ import { Link } from "react-router-dom";
 import useGlobalReducer from "../hooks/useGlobalReducer";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { object } from "prop-types";
 
 export const Card = ({ element, type }) => {
   const { store, dispatch } = useGlobalReducer();
 
+  let traits = {};
+
+  if (type === "people") {
+    traits = {
+      hair_color: element.properties.hair_color,
+      eye_color: element.properties.eye_color,
+    };
+  }
+
+  if (type === "planets") {
+    traits = {
+      climate: element.properties.climate,
+      terrain: element.properties.terrain,
+    };
+  }
+
+  if (type === "vehicles") {
+    traits = {
+      model: element.properties.model,
+      manufacturer: element.properties.manufacturer,
+    };
+  }
+  if (type === "species") {
+    traits = {
+      classification: element.properties.classification,
+      designation: element.properties.designation,
+    };
+  }
+  if (type === "starships") {
+    traits = {
+      model: element.properties.model,
+      crew: element.properties.crew,
+    };
+  }
+
   return (
-    <div className="card text-light bg-dark" style={{ width: "18rem" }}>
-      <h5 className="card-title">{element.name}</h5>
+    <div
+      className="card text-light bg-dark d-flex flex-column"
+      style={{ width: "18rem" }}
+    >
+      <h2 className="card-title mt-3">{element.properties.name}</h2>
       <img
         src={`https://github.com/breatheco-de/swapi-images/blob/master/public/images/${type}/${element.uid}.jpg?raw=true`}
-        className="card-img-top"
-        alt={`No image found for ${element.name}`}
+        className="card-img mt-2"
+        alt={`No image found for ${element.properties.name}`}
       />
-      <div className="card-body">
-        <div className="d-flex justify-content-between">
+      <div
+        className="card-body text-start d-flex flex-column"
+        style={{ flex: 1, minHeight: 0 }}
+      >
+        <div style={{ flex: 1 }}>
+          {Object.entries(traits).map(([key, value]) => (
+            <div key={key} className="mb-2">
+              <strong>{key}:</strong> {String(value)}
+            </div>
+          ))}
+        </div>
+
+        <div className="d-flex justify-content-between mt-auto pt-3">
           <Link to={`/${type}/${element.uid}`} className="btn btn-primary">
             See more
           </Link>
